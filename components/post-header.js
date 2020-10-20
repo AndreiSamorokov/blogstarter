18760ce4d15126b3e5964cfaa046ddff5b1b8ab7
AndreiSamorokov <andreiv0202@gmail.com>
2020-10-21 02:20:45 +0900

    changed blog page

--- components/post-header.js
+++ components/post-header.js
@@ -6,17 +6,11 @@ import PostTitle from '../components/post-title'
 export default function PostHeader({ title, coverImage, date, author }) {
   return (
     <>
-      <PostTitle>{title}</PostTitle>
-      <div className="hidden md:block md:mb-12">
-        <Avatar name={author.name} picture={author.picture} />
-      </div>
+      <PostTitle>{title}</PostTitle> 
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
       </div>
-      <div className="max-w-2xl mx-auto">
-        <div className="block md:hidden mb-6">
-          <Avatar name={author.name} picture={author.picture} />
-        </div>
+      <div className="max-w-2xl mx-auto"> 
         <div className="mb-6 text-lg">
           <DateFormatter dateString={date} />
         </div>
