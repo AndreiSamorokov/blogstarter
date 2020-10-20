406c7e7f4dde7c18a09981bf0f644b25ae740e78
AndreiSamorokov <andreiv0202@gmail.com>
2020-10-21 02:13:47 +0900

    new update

--- components/intro.js
+++ components/intro.js
@@ -4,17 +4,10 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h5 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Successfull Business
+        * Andrei *
       </h5>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
-        <a
-          href="https://nextjs.org/"
-          className="underline hover:text-success duration-200 transition-colors"
-        >
-          Next.js
-        </a>{' '}
-        and {CMS_NAME}.
+        Make your business to success with me!        
       </h4>
     </section>
   )
